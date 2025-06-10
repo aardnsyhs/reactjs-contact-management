3ff2a14f4733039b42408e50f724ba7e57892423
aardnsyhs <ardiansyahdian755@gmail.com>
2025-06-10 15:51:18 +0700

    feat: add user login functionality with form handling and API integration

--- src/components/lib/api/UserApi.js
+++ src/components/lib/api/UserApi.js
@@ -8,3 +8,14 @@ export const userRegister = async ({ username, name, password }) => {
     body: JSON.stringify({ username, name, password }),
   });
 };
+
+export const userLogin = async ({ username, password }) => {
+  return await fetch(`${import.meta.env.VITE_API_PATH}/users/login`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+};
