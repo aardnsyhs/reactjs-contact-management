b29170d928cd1eb55623d1ea40b60531e70caa38
aardnsyhs <ardiansyahdian755@gmail.com>
2025-06-11 08:15:08 +0700

    feat: add DashboardLayout component with navigation links and layout structure

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 import UserRegister from "./components/User/UserRegister";
 import UserLogin from "./components/User/UserLogin";
+import DashboardLayout from "./components/DashboardLayout";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")).render(
           <Route path="/register" element={<UserRegister />} />
           <Route path="/login" element={<UserLogin />} />
         </Route>
-        <Route path="/dashboard"></Route>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="contacts" element={<div>Contacts</div>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
